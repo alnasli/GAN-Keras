d8f7b1bb7a9b790a92c7ac90bb7d74e93b9becd4
Nasli-EXT <uib06143@contiwan.com>
2018-12-04 22:48:39 +0100

    build discriminator func

--- gan.py
+++ gan.py
@@ -7,3 +7,22 @@ from keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def build_discriminator(img_shape):
+    """
+    Build the discriminator
+    :param img_shape: input shape of the discriminator
+    :return: discriminator, keras model
+    """
+    img=Input(shape=img_shape)
+    X=Flatten(input_shape=img_shape)(img)
+    X=Dense(512)(X)
+    X=LeakyReLU(alpha=0.2)(X)
+    X=Dense(256)(X)
+    X=LeakyReLU(alpha=0.2)(X)
+    validity=Dense(1, activation='sigmoid')(X)
+
+    discriminator=Model(img,validity)
+    discriminator.summary()
+    return discriminator

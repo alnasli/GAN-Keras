316b536796c2a7f299d8dc21d2337df74387cdde
Nasli-EXT <uib06143@contiwan.com>
2018-12-04 22:48:08 +0100

    import libraries

---
+++ gan.py
@@ -0,0 +1,9 @@
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten
+from keras.layers import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import  Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+import numpy as np

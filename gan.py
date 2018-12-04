a71fa44207ec6fb54ee632e4150b15279c7c61ee
Nasli-EXT <uib06143@contiwan.com>
2018-12-04 22:49:16 +0100

    build generator func

--- gan.py
+++ gan.py
@@ -26,3 +26,30 @@ def build_discriminator(img_shape):
     discriminator=Model(img,validity)
     discriminator.summary()
     return discriminator
+
+def build_generator(latent_dim,img_shape):
+    """
+    Build the generator
+    :param latent_dim: dimension of the input noise
+    :param img_shape:
+    :return: generator model
+    """
+
+    noise=Input(shape=(latent_dim,)) #generated noise
+
+    #GAN layers
+    X=Dense(256, input_dim=latent_dim)(noise)
+    X=LeakyReLU(alpha=0.2)(X)
+    X=BatchNormalization(momentum=0.8)(X)
+    X=Dense(512)(X)
+    X=LeakyReLU(alpha=0.2)(X)
+    X=BatchNormalization(momentum=0.8)(X)
+    X=Dense(1024)(X)
+    X=LeakyReLU(alpha=0.2)(X)
+    X=BatchNormalization(momentum=0.8)(X)
+    X=Dense(np.prod(img_shape), activation='tanh')(X)
+
+    img=Reshape(img_shape)(X) #output of the generator
+    generator=Model(noise,img)
+    generator.summary()
+    return generator
